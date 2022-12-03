53abaaf7c101ba251371dfd49a03d20db0f78f8f
driedwater <210288D@mymail.nyp.edu.sg>
2022-12-03 18:00:29 +0800

    logging for account

--- server/lovelace/app.py
+++ server/lovelace/app.py
@@ -1,5 +1,11 @@
 from flask import Flask,request,jsonify,Blueprint
 from account import account_page
+import os, logging
+
+logger = logging.getLogger('werkzeug')
+logger.setLevel(logging.INFO)
+logger.addHandler(logging.FileHandler(f'{os.getcwd()}\\server\\lovelace\\logs\\werkzeug.log'))
+
 app = Flask(__name__)
 app.register_blueprint(account_page)
 app.run()
