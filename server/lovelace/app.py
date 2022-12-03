b556b5eae6eb988b1453a18075380c5ec12bad7d
Cadenzar <88576308+Cadenazar@users.noreply.github.com>
2022-12-03 18:00:27 +0800

    Add files via upload

--- server/lovelace/app.py
+++ server/lovelace/app.py
@@ -1,5 +1,9 @@
 from flask import Flask,request,jsonify,Blueprint
 from account import account_page
+from recommendation import recommendation_page
+from chat import chat_page
 app = Flask(__name__)
 app.register_blueprint(account_page)
+app.register_blueprint(recommendation_page)
+app.register_blueprint(chat_page)
 app.run()
