b1678611379a59cf4e6fd540d5bb3ad1f81b6d3c
Jon1oh <jon.loh@hotmail.com>
2022-12-04 09:53:05 +0800

    changes file/folder structue to layer-first structure

---
+++ client/lovelace/lib/utils/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const primaryColor = Color.fromRGBO(128, 0, 128, 1);
+const selectedIconColor = Color.fromRGBO(255, 255, 255, 1);
+const unselectedIconColor = Color.fromRGBO(128, 128, 128, 1);
+const mobileBackgroundColor = Color.fromRGBO(0, 0, 0, 1);
