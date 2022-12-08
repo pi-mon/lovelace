da3272d7c20fd903ae68ad183aa17a695e4be16d
Cadenzar <88576308+Cadenazar@users.noreply.github.com>
2022-12-08 16:04:28 +0800

    Add files via upload

--- server/lovelace/models/account_model.py
+++ server/lovelace/models/account_model.py
@@ -1,9 +1,9 @@
 class User:
   
-  def __init__(self,username,password,email):
-    self._id = email
+  def __init__(self,password,email):
+    self.email = email
     self.password = password
-    self.username = username
+    self.username = ""
 
 class User_details:
 
