e37b23214e4bdefd6e8f6ca130e36431c1272be0
lgf2111 <lgf2111@gmail.com>
2022-12-09 17:43:23 +0800

    implement docker

--- server/lovelace/app.py
+++ server/lovelace/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, request, jsonify, Blueprint
+import os
+from flask import Flask, request, jsonify, Blueprint, redirect
 from logger import setup_logger
 from account import account_page
 from recommendation import recommendation_page
@@ -6,7 +7,15 @@ from chat import chat_page
 
 logger = setup_logger("")
 app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return redirect("https://github.com/lgf2111/lovelace")
+
+
 app.register_blueprint(account_page)
 app.register_blueprint(recommendation_page)
 app.register_blueprint(chat_page)
-app.run(debug=True)
+host = "0.0.0.0" if os.environ.get("IN_DOCKER", False) else "localhost"
+app.run(debug=True, host=host, port=3000)
