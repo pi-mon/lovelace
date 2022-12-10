1bb4ceaa6c040a09a85e3a97d0910070ad4ebc63
driedwater <88548136+driedwater@users.noreply.github.com>
2022-12-10 19:45:24 +0800

    added logging back to account

--- server/lovelace/account/routes.py
+++ server/lovelace/account/routes.py
@@ -24,6 +24,7 @@ def create_account():
     new_email = account_json["email"]
     new_password = account_json["password"]
     if not new_email or not new_password or email_validation(new_email) or password_validation(new_password):  # check if empty input
+        logger.info("%s Did not succeed in creating an account due to failed input validation", request.remote_addr)
         return jsonify({"creation": False, "response": "Invalid email or password"})
     else:
         try:
@@ -32,13 +33,16 @@ def create_account():
             new_user_json = new_user.__dict__
             account_collection.user.create_index("email", unique=True)
             account_collection.user.insert_one(new_user_json)
+            logger.info("%s Succeed in creating an account using email %s", request.remote_addr, new_email)
             return jsonify(
                 {"creation": True, "response": "Account was created successfully"}
             )
         except db_errors.DuplicateKeyError:
+            logger.info("%s Did not succeed in creating an account due to duplicated email %s", request.remote_addr, new_email)
             return jsonify({"creation": False, "response": "Email already exist"})
 
         except argon2_exceptions.VerifyMismatchError:
+            logger.info("%s Did not succeed in creating an account due to argon2 exceptions mismatch", request.remote_addr)
             return jsonify({"login":False, "response":"Invalid email or password"})
 
 
@@ -51,6 +55,7 @@ def login_account():
     password = account_json["password"]
     if not email or not password or email_validation(email) or password_validation(password):  # check if empty input
         account_collection = mongo.account
+        logger.info("%s Failed to login using email %s due to failed input validation", request.remote_addr, email)
         return jsonify({"login": False, "response": "Invalid email or password"})
     account_collection = mongo.account
     try:
@@ -62,6 +67,7 @@ def login_account():
         )  # compares password hash
     except TypeError:
         if account_collection.user.find_one({"email": email}, {"password": 1}) == None:
+            logger.info("%s Failed to login using email %s", request.remote_addr, email)
             return jsonify({"login": False, "response": "Invalid email or password"})
     if valid_login:
         token = jwt.encode(
@@ -71,6 +77,7 @@ def login_account():
         )
         # resp = make_response(jsonify({"login":True,"response":"User login successful"}))
         # resp.set_cookie("token", token)
+        logger.info("%s Logged in successfully using email %s", request.remote_addr, email)
         return jsonify(
             {"login": True, "response": "User login successful", "token": token}
         )
