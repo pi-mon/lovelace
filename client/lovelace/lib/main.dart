cfa096f5c5a6f6648dc7acb84adff301db2bb731
Jon1oh <jon.loh@hotmail.com>
2022-12-13 00:10:25 +0800

    Prevent screenshots and screen recording

--- client/lovelace/lib/main.dart
+++ client/lovelace/lib/main.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:lovelace/responsive/mobile_screen_layout.dart';
 import 'package:lovelace/responsive/responsive_layout.dart';
@@ -7,9 +8,11 @@ import 'package:lovelace/screens/landing/guest_landing_screen.dart';
 import 'package:lovelace/utils/colors.dart';
 import 'package:flutter/services.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter_windowmanager/flutter_windowmanager.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  FlutterWindowManager.addFlags(FlutterWindowManager.FLAG_SECURE); // TODO: FIND OUT HOW TO TEST
   final preferences = await SharedPreferences.getInstance();
   final isLoggedIn = preferences.getBool('isLoggedIn') ?? false;
 
@@ -26,11 +29,12 @@ void main() async {
   runApp(MyApp(isLoggedIn: isLoggedIn));
 }
 
+// ignore: must_be_immutable
 class MyApp extends StatefulWidget {
-      final bool isLoggedIn;
-    final _userPages = const ResponsiveLayout(
-        mobileScreenLayout: MobileScreenLayout(),
-        webScreenLayout: WebScreenLayout());
+  final bool isLoggedIn;
+  final _userPages = const ResponsiveLayout(
+      mobileScreenLayout: MobileScreenLayout(),
+      webScreenLayout: WebScreenLayout());
 
   const MyApp({Key? key, required this.isLoggedIn}) : super(key: key);
 
--- client/lovelace/windows/flutter/generated_plugin_registrant.cc
+++ client/lovelace/windows/flutter/generated_plugin_registrant.cc
@@ -9,6 +9,8 @@
 #include <flutter_secure_storage_windows/flutter_secure_storage_windows_plugin.h>
 #include <local_auth_windows/local_auth_plugin.h>
 #include <network_info_plus/network_info_plus_windows_plugin.h>
+#include <screen_retriever/screen_retriever_plugin.h>
+#include <window_manager/window_manager_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   FlutterSecureStorageWindowsPluginRegisterWithRegistrar(
@@ -17,4 +19,8 @@ void RegisterPlugins(flutter::PluginRegistry* registry) {
       registry->GetRegistrarForPlugin("LocalAuthPlugin"));
   NetworkInfoPlusWindowsPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("NetworkInfoPlusWindowsPlugin"));
+  ScreenRetrieverPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("ScreenRetrieverPlugin"));
+  WindowManagerPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("WindowManagerPlugin"));
 }
