751802e91c08c6492ef293ce3f8cd345411e23f8
unknown <=>
2022-12-13 11:54:35 +0800

    create admin

---
+++ server/lovelace/admin/routes.py
@@ -0,0 +1,12 @@
+from flask import Blueprint, jsonify, request
+from lovelace.account.utils import token_required
+from lovelace.admin.utils import admin_required
+
+
+admin_page = Blueprint("admin", __name__, template_folder="templates")
+
+@admin_page.route("/admin")
+@token_required
+@admin_required
+def admin_dashboard():
+  return(jsonify({"login":True,"response":"User has logged in as admin sucessfully"}))
