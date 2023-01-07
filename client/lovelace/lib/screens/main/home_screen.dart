5236fcb8697d7a6625081faefaeeb014183fd8cd
Atlas2212 <60185645+Atlas2212@users.noreply.github.com>
2023-01-07 19:16:00 +0100

    superkey replacement

--- client/lovelace/lib/screens/main/home_screen.dart
+++ client/lovelace/lib/screens/main/home_screen.dart
@@ -4,7 +4,7 @@ import 'package:lovelace/widgets/display_card.dart';
 import 'package:lovelace/widgets/cards_stack_widget.dart';
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+  const HomeScreen({super.key});
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
