264c798a9114d03b57433b877590f39df2cf6304
Jon1oh <jon.loh@hotmail.com>
2023-01-21 12:08:41 +0800

    create encrpytion/decryption functions

---
+++ client/lovelace/lib/resources/e2e/encryption_methods.dart
@@ -0,0 +1,20 @@
+import 'package:encrypt/encrypt.dart';
+
+class EncryptionDecryption {
+  static final key = Key.fromSecureRandom(32); // in bytes
+  static final iv  = IV.fromSecureRandom(16); // in bytes
+  static final encrypter = Encrypter(AES(key));
+
+  Future<Encrypted> encryptAES(dynamic plainText) async  {
+    final encrypted = encrypter.encrypt(plainText, iv: iv);
+    print(encrypted.bytes);
+    print(encrypted.base16);
+    print(encrypted.base64);
+    return encrypted;
+  }
+
+  Future<dynamic> decryptAES(Encrypted cipherText) async {
+    final decrypted = encrypter.decrypt(cipherText, iv: iv);
+    return decrypted;
+  }
+}
