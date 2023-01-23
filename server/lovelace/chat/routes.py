3aaf5b6c3f3ad973e025b8564e0308097c6bd980
driedwater <88548136+driedwater@users.noreply.github.com>
2023-01-23 12:13:00 -0500

    chat send encryption key to client

--- server/lovelace/chat/routes.py
+++ server/lovelace/chat/routes.py
@@ -3,6 +3,7 @@ from lovelace import chat_logger as logger
 from lovelace import socketio
 from flask_socketio import join_room, emit, leave_room
 from lovelace.account.utils import token_required
+import secrets
 from lovelace import (
     mongo_account_read,
     mongo_account_write,
@@ -11,6 +12,7 @@ from lovelace import (
     mongo_temp_read,
     mongo_chat_write,
 )
+
 
 chat = Blueprint("chat", __name__, template_folder="templates")
 
@@ -49,13 +51,22 @@ def join(_, message):
 
     if room == None:
         room = chat_collection.chat.insert_one(
-            {"user1": message["user1"], "user2": message["user2"]}
+            {"user1": message["user1"], "user2": message["user2"], "total_user":1}
         )
+    else:
+        chat_collection.chat.update_one({"$and": [{"user1": user1}, {"user2": user2}]}, {"$set": {"total_user":2}})
 
     # join room
     room_name = str(room["_id"])
     join_room(str(room_name))
     response = f"{user1} has entered room ({room_name})."
+
+    # get number of users
+    user_count = chat_collection.chat.find_one({"$and": [{"user1": user1}, {"user2": user2}]})["total_user"]
+    if user_count == 2:
+        key = secrets.token_urlsafe(32)
+        emit("message", {"key": key}, room_name=room_name)
+
     print(response)
 
     # Emit message or notifier to other user of same room
@@ -88,4 +99,5 @@ def leave(_, message):
     )["_id"]
     # leaving the room
     leave_room(room=room)
+    chat_collection.chat.delete_one({"_id":room})
     emit("message", {"response": f"{user1} has left the room."}, room=room)
