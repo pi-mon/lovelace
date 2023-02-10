30e9ecc37fe8c0eacfba76ced1e643a08e3ff314
Cadenzar <88576308+Cadenazar@users.noreply.github.com>
2023-02-10 15:34:02 +0800

    Add files via upload

--- server/lovelace/__init__.py
+++ server/lovelace/__init__.py
@@ -10,6 +10,8 @@ import dotenv
 from prometheus_flask_exporter import PrometheusMetrics
 from flask_httpauth import HTTPBasicAuth
 import flask_monitoringdashboard as dashboard
+from flask_talisman import Talisman
+from flask_wtf.csrf import CSRFProtect
 
 dotenv.load_dotenv()
 ca = certifi.where()
@@ -50,24 +52,42 @@ recommendation_logger = setup_logger("recommendation")
 
 
 app = Flask(__name__)
+app.config["WTF_CSRF_SECRET_KEY"] = os.environ.get("APPLICATION_SIGNATURE_KEY")
+Talisman(app,force_https=False)
+
+#csrf = CSRFProtect(app)
 auth = HTTPBasicAuth(app)
 metrics = PrometheusMetrics(app, metrics_decorator=auth.login_required)
 metrics.register_default(
     metrics.counter(
-        'by_path_counter', 'Request count by request paths',
-        labels={'path': lambda: request.path}
+        "by_path_counter",
+        "Request count by request paths",
+        labels={"path": lambda: request.path},
     )
 )
+#csrf.init_app(app)
+
+
 @auth.verify_password
 def verify_credentials(username, password):
-    return (username, password) == (os.environ.get("METRICS_USERNAME"), os.environ.get("METRICS_PASSWORD"))
+    return (username, password) == (
+        os.environ.get("METRICS_USERNAME"),
+        os.environ.get("METRICS_PASSWORD"),
+    )
+
+
 app.config.from_pyfile("config.py")
 
 dashboard.config.init_from(file="monitor/config.cfg")
 dashboard.bind(app)
 
-limiter = Limiter(app, key_func=get_remote_address, default_limits=["10 per minute"])
-socketio = SocketIO(app, cors_allowed_origins=["127.0.0.1", "10.0.2.2"])
+limiter = Limiter(get_remote_address, app=app, default_limits=["10 per minute"])
+socketio = SocketIO(
+    app,
+    cors_allowed_origins=[
+        "ec2-13-229-224-40.ap-southeast-1.compute.amazonaws.com",
+    ],
+)
 
 from lovelace.account.routes import account_page
 from lovelace.recommendation.routes import recommendation
